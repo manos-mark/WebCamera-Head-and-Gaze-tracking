e307e3e7a0bb9be6503e10dd9201a46598817388
manos <manos-mark@hotmail.com>
2019-07-19 12:06:56 +0300

    fix

---
+++ UnityProject/Assets/WebCameraHeadAndGazeTracking/Scripts/CameraFollowCar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowCar : MonoBehaviour
+{
+    public Transform follow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate() {
+        transform.position = follow.position;
+    }
+}
--- UnityProject/Assets/WebCameraHeadAndGazeTracking/Scripts/RotateCameraOnHeadMove.cs
+++ UnityProject/Assets/WebCameraHeadAndGazeTracking/Scripts/RotateCameraOnHeadMove.cs
@@ -4,12 +4,16 @@ using System.Globalization;
 public class RotateCameraOnHeadMove : MonoBehaviour
 {
     private FaceDetector _faceDetector;
-
     private ScriptConnector _scriptConnector;
+    public GameObject camera;
+
+    private static int MIN_DETECTED_ROTATION = 4;
+    private static int MAX_DETECTED_ROTATION = 10;
+    private static float ROTATION_SPEED = 0.5f;
 
     private float previousYRotation = 0;
     private float currentYRotation = 0;
-    private float receivedYRotation = 0;
+    private float headYRotation = 0;
     private int currentNullRotationCount = 0;
     
     private void Start()
@@ -18,59 +22,55 @@ public class RotateCameraOnHeadMove : MonoBehaviour
         _scriptConnector = new ScriptConnector();
     }
 
-    private static int MIN_DETECTED_ROTATION = 4;
-    private static int MAX_DETECTED_ROTATION = 10;
-    private static float ROTATION_SPEED = 0.5f;
-
     private void Update()
     {
-        Camera cam = GameObject.Find("Camera").GetComponent<Camera>();
         string message = _scriptConnector.ReceiveMessage();
 
         if (message != null)
         {
             currentNullRotationCount = 0;
-            this.receivedYRotation = float.Parse(message, CultureInfo.InvariantCulture.NumberFormat);
+            this.headYRotation = float.Parse(message, CultureInfo.InvariantCulture.NumberFormat);
             
-            if (receivedYRotation >= previousYRotation + MIN_DETECTED_ROTATION) // rotate right
+            if (headYRotation >= previousYRotation + MIN_DETECTED_ROTATION) // rotate right
             {
-
-                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion(0, ++currentYRotation, 0, 0);
-                cam.transform.rotation =  Quaternion.Slerp(transform.rotation, targetRotation, ROTATION_SPEED *  Time.deltaTime);
-                previousYRotation = targetRotation.y;
+                ++currentYRotation;
+                rotateCamera(currentYRotation, ROTATION_SPEED);
+                previousYRotation = currentYRotation;
             } 
-            else if (receivedYRotation < previousYRotation - MIN_DETECTED_ROTATION) // rotate left
+            else if (headYRotation < previousYRotation - MIN_DETECTED_ROTATION) // rotate left
             {
-                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion(0, --currentYRotation, 0, 0);
-                cam.transform.rotation =  Quaternion.Slerp(transform.rotation, targetRotation, ROTATION_SPEED *  Time.deltaTime);
-                previousYRotation = targetRotation.y;
+                --currentYRotation;
+                rotateCamera(currentYRotation, ROTATION_SPEED);
+                previousYRotation = currentYRotation;
             }
         } 
         else { 
             currentNullRotationCount++; 
             if (currentNullRotationCount > 50) // if cannot detect face for 50 frames then recenter
             {
-                UnityEngine.Quaternion targetRotation;
                 if (currentYRotation == 0) return;
                 if (currentYRotation > 1) {
                     currentYRotation -= 1;
-                    targetRotation = new UnityEngine.Quaternion(0, currentYRotation, 0, 0);
-                    cam.transform.rotation =  Quaternion.Slerp(transform.rotation, targetRotation, ROTATION_SPEED *  Time.deltaTime);
+                   rotateCamera(currentYRotation, ROTATION_SPEED);
                 } 
                 else if (currentYRotation < -1)
                 {
                     currentYRotation +=1;
-                    targetRotation = new UnityEngine.Quaternion(0, currentYRotation, 0, 0);
-                    cam.transform.rotation =  Quaternion.Slerp(transform.rotation, targetRotation, ROTATION_SPEED *  Time.deltaTime);
+                    rotateCamera(currentYRotation, ROTATION_SPEED);
                 }
                 else 
                 {
-                    targetRotation = new UnityEngine.Quaternion(0, 0, 0, 0);
-                    cam.transform.rotation =  Quaternion.Slerp(transform.rotation, targetRotation, ROTATION_SPEED *  Time.deltaTime);
+                    rotateCamera(0, ROTATION_SPEED);
                     currentYRotation = 0;
                 }
             }
         } 
+    }
+
+    private void rotateCamera(float yRotation, float rotationSpeed)
+    {
+        UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion(0, yRotation, 0, 0);
+        this.camera.transform.rotation =  Quaternion.Slerp(this.camera.transform.rotation, targetRotation, rotationSpeed *  Time.deltaTime);
     }
 
     private void OnDestroy()
