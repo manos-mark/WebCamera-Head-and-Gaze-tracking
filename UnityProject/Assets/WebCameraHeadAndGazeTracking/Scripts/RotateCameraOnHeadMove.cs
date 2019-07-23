6790560a68cc67c99c25d44bd8f8d56df8c45ae0
manos <manos-mark@hotmail.com>
2019-07-23 17:36:02 +0300

    remove rotation_distance from params

--- UnityProject/Assets/WebCameraHeadAndGazeTracking/Scripts/RotateCameraOnHeadMove.cs
+++ UnityProject/Assets/WebCameraHeadAndGazeTracking/Scripts/RotateCameraOnHeadMove.cs
@@ -5,18 +5,18 @@ public class RotateCameraOnHeadMove : MonoBehaviour
 {
     private FaceDetector _faceDetector;
     private ScriptConnector _scriptConnector;
-    public GameObject camera;
+    public Transform Target;
 
-    private static int MIN_DETECTED_ROTATION = 4;
-    private static int MAX_DETECTED_ROTATION = 10;
-    private static float ROTATION_SPEED = 0.5f;
+    public int MIN_DETECTED_ROTATION = 4;
+    public float ROTATION_SPEED = 1.5f;
+    public float ROTATION_DISTANCE = 1.5f;
 
     private float previousYRotation = 0;
     private float currentYRotation = 0;
     private float headYRotation = 0;
     private int currentNullRotationCount = 0;
     
-    private void Start()
+    private void Awake()
     {
         _faceDetector = new FaceDetector(Application.dataPath);
         _scriptConnector = new ScriptConnector();
@@ -33,14 +33,14 @@ public class RotateCameraOnHeadMove : MonoBehaviour
             
             if (headYRotation >= previousYRotation + MIN_DETECTED_ROTATION) // rotate right
             {
-                ++currentYRotation;
-                rotateCamera(currentYRotation, ROTATION_SPEED);
+                currentYRotation = currentYRotation + ROTATION_DISTANCE;
+                rotateCamera(currentYRotation);
                 previousYRotation = currentYRotation;
             } 
             else if (headYRotation < previousYRotation - MIN_DETECTED_ROTATION) // rotate left
             {
-                --currentYRotation;
-                rotateCamera(currentYRotation, ROTATION_SPEED);
+                currentYRotation = currentYRotation - ROTATION_DISTANCE;
+                rotateCamera(currentYRotation);
                 previousYRotation = currentYRotation;
             }
         } 
@@ -51,26 +51,26 @@ public class RotateCameraOnHeadMove : MonoBehaviour
                 if (currentYRotation == 0) return;
                 if (currentYRotation > 1) {
                     currentYRotation -= 1;
-                   rotateCamera(currentYRotation, ROTATION_SPEED);
+                   rotateCamera(currentYRotation);
                 } 
                 else if (currentYRotation < -1)
                 {
                     currentYRotation +=1;
-                    rotateCamera(currentYRotation, ROTATION_SPEED);
+                    rotateCamera(currentYRotation);
                 }
                 else 
                 {
-                    rotateCamera(0, ROTATION_SPEED);
+                    rotateCamera(0);
                     currentYRotation = 0;
                 }
             }
         } 
     }
 
-    private void rotateCamera(float yRotation, float rotationSpeed)
+    private void rotateCamera(float yRotation)
     {
         UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion(0, yRotation, 0, 0);
-        this.camera.transform.rotation =  Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed *  Time.deltaTime);
+        this.transform.localRotation = Quaternion.Euler(0, yRotation * ROTATION_SPEED, 0);
     }
 
     private void OnDestroy()
